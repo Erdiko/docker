9c96097fb00b75316d0bc54c9b7659d0f9f80a96
john arroyo <john.arroyo@gmail.com>
2017-02-28 15:24:16 -0800

    flush out more of the lamp stack

--- lamp/scripts/mysql.sh
+++ lamp/scripts/mysql.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 # Install MySQL
+#iptables -I INPUT -p tcp --dport 3306 -m state --state NEW,ESTABLISHED -j ACCEPT
+#iptables -I OUTPUT -p tcp --sport 3306 -m state --state ESTABLISHED -j ACCEPT
 
+service mysql start
--- lamp/scripts/nginx.sh
+++ lamp/scripts/nginx.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 # Install nginx
+/etc/init.d/nginx start
