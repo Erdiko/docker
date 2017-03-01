553f04f45f21cf7ff3c3b8130967bf1e6e1888b7
john arroyo <john.arroyo@gmail.com>
2017-02-28 16:03:53 -0800

    add mysql 5.6 script

---
+++ lamp/scripts/mysql-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt-get install -y lsb-release
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+dpkg -i mysql-apt-config_0.7.3-1_all.deb
+apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
+apt-get update
+apt-get install -y mysql-server
